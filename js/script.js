2d6b015e08f7a026a707031230c5c041c93bad16
Maksym Heraskevych <heraskevych.maksym@gmail.com>
2019-03-05 04:44:10 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(window).scroll(function() {
     $('.mov').each(function(){
       var imagePos = $(this).offset().top;
       var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+500) {
+      if (imagePos < topOfWindow+650) {
         $(this).addClass('bounceInRight');
       }
     });
